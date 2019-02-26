ddea94d368b6f4293b41b483ca2e8c68c7b0c662
a910692370 <910692370@qq.com>
2019-02-26 17:42:24 +0800

    C# Graphics绘制验证码

---
+++ Graphics绘制验证码/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Graphics绘制验证码
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb1 = new System.Windows.Forms.Label();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lb1
+            // 
+            this.lb1.AutoSize = true;
+            this.lb1.Location = new System.Drawing.Point(47, 62);
+            this.lb1.Name = "lb1";
+            this.lb1.Size = new System.Drawing.Size(53, 12);
+            this.lb1.TabIndex = 0;
+            this.lb1.Text = "验证码：";
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(106, 28);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(180, 80);
+            this.pb1.TabIndex = 1;
+            this.pb1.TabStop = false;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(310, 57);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 23);
+            this.btn1.TabIndex = 2;
+            this.btn1.Text = "刷新";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 150);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.lb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb1;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ Graphics绘制验证码/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graphics绘制验证码
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CodeImage(CheckCode());
+        }
+        /// <summary>
+        /// 生成随机验证码
+        /// </summary>
+        /// <returns></returns>
+        private string CheckCode()
+        {
+            int num;
+            char code;
+            string str = "";
+            Random rd = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                num = rd.Next();
+                if (num % 2 == 0)
+                    code = (char)('0' + (char)(num % 10));
+                else
+                    code = (char)('A' + (char)(num % 26));
+                str += code.ToString();
+            }
+            return str;
+        }
+        private void CodeImage(string a)
+        {
+            if (a.Trim() == "")
+            {
+                MessageBox.Show("生成验证码失败！");
+                return;
+            }
+            Bitmap image = new Bitmap(180, 80);
+            Graphics g = Graphics.FromImage(image);
+
+            Random rd = new Random();
+            g.Clear(Color.White);
+            g.DrawString(a, new Font("楷体", 60, FontStyle.Italic), new SolidBrush(Color.Red), -10, 0);
+            //绘制随机干扰线段
+            for (int i = 0; i < 20; i++)
+            {
+                int x1 = rd.Next(image.Width);
+                int x2 = rd.Next(image.Width);
+                int y1 = rd.Next(image.Height);
+                int y2 = rd.Next(image.Height);
+                g.DrawLine(new Pen(Color.Black), x1, y1, x2, y2);
+            }
+            //绘制随机干扰点
+            for (int i = 0; i < 500; i++)
+            {
+                int x1 = rd.Next(image.Width);
+                int y1 = rd.Next(image.Height);
+                g.DrawLine(new Pen(Color.Black), x1, y1, (x1 + 1), y1);
+            }
+            pb1.BackgroundImageLayout = ImageLayout.Center;
+            pb1.BackgroundImage = image;
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            CodeImage(CheckCode());
+        }
+    }
+}
